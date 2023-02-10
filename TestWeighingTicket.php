6a7a9418724b3afeb15efee4f22aca5fd7b91d95
TOM SIKORA <siko0001@2A4F1-31UPC064.ad-urca.univ-reims.fr>
2023-02-10 11:14:07 +0100

    Exercice 2

--- TestWeighingTicket.php
+++ TestWeighingTicket.php
@@ -17,3 +17,34 @@ $pommeDT -> print();
 # Il dépend donc du prix au kilogramme et du poids.
 
 # Question 4
+echo "Attributs de l'objet pommeDT : \n";
+echo "Nom : {$pommeDT->getArticleName()}\n";
+echo "Prix au kilo : {$pommeDT->getPricePerKilogram()}\n";
+echo "Poids : {$pommeDT->getWeight()}\n";
+echo "Prix : {$pommeDT->getPrice()}\n";
+
+# Question 5
+# Non, car l'attribut price dépend de 2 autres attributs (poids et prix au kilo).
+# price ne peut donc pas être modifié.
+
+# Modification de l'attribut articleName
+echo "{$pommeDT->getArticleName()}\n";
+$pommeDT->setArticleName('Tomate');
+echo "{$pommeDT->getArticleName()}\n";
+
+# Modification de l'attribut pricePerKilogram
+echo "{$pommeDT->getPricePerKilogram()}\n";
+$pommeDT->setPricePerKilogram(10.38);
+echo "{$pommeDT->getPricePerKilogram()}\n";
+
+# Modification de l'attribut weight
+echo "{$pommeDT->getWeight()}\n";
+$pommeDT->setWeight(200);
+echo "{$pommeDT->getWeight()}\n";
+
+# La méthode d'instance qu'il faut définir dans la classe WeighingTicket est __toString.
+echo "$inconnu\n";
+
+# Question 6
+# La méthode weighing permet de tirer au hasard un nombre entre 0 et 5000, elle affecte
+# ce nombre à l'attribut weight de l'objet qui utilise cette méthode.
--- WeighingTicket.php
+++ WeighingTicket.php
@@ -43,4 +43,106 @@ class WeighingTicket
         echo "Prix : {$this->price}\n" ;
     }
 
+    /**
+     * Accesseur au nom de l'article. Retourne la valeur du nom sous forme de chaîne de
+     * caractères.
+     *
+     * @return string Nom de l'article
+     */
+    public function getArticleName() : string
+    {
+        return $this->articleName;
+    }
+
+    /**
+     * Accesseur au prix au kilogramme de l'article. Retourne la valeur du prix
+     * sous forme de nombre à virgule.
+     *
+     * @return float Prix au kilogramme de l'article
+     */
+    public function getPricePerKilogram() : float
+    {
+        return $this->pricePerKilogram;
+    }
+
+    /**
+     * Accesseur au poids de l'article. Retourne la valeur du poids sous forme de nombre entier.
+     *
+     * @return int Poids de l'article
+     */
+    public function getWeight() : int
+    {
+        return $this->weight;
+    }
+
+    /**
+     * Accesseur au prix total de l'article. Retourne la valeur du poids sous forme de
+     * nombre à virgule.
+     *
+     * @return float Prix total de l'article
+     */
+    public function getPrice() : float
+    {
+        return $this->price;
+    }
+
+    /**
+     * Modificateur au nom de l'article. Permet d’affecter un nouveau nom à l'article.
+     *
+     * @param string $articleName Nom de l'article
+     */
+    public function setArticleName(string $articleName) : void
+    {
+        $this->articleName = $articleName;
+    }
+
+    /**
+     * Modificateur au prix au kilo de l'article. Permet d’affecter
+     * un nouveau prix au kilo à l'article.
+     *
+     * @param float $pricePerKilogram Prix au kilo de l'article
+     */
+    public function setPricePerKilogram(float $pricePerKilogram) : void
+    {
+        $this->pricePerKilogram = $pricePerKilogram;
+    }
+
+    /**
+     * Modificateur au poids de l'article. Permet d’affecter un nouveau poids à l'article.
+     *
+     * @param int $weight Poids de l'article
+     */
+    public function setWeight(int $weight) : void
+    {
+        $this->weight = $weight;
+    }
+
+    /**
+     * Affichage des attributs de l'article. Retourne un tableau composé du nom, du prix au
+     * kilo, du poids et du prix final de l'article.
+     *
+     * @return string Attributs d'article
+     */
+    public function __toString() : string
+    {
+        $res = "********************\n";
+        $res = $res."*  Article      : {$this->articleName}\n";
+        $res = $res."*  Prix au kilo : {$this->pricePerKilogram}\n";
+        $res = $res."*  Poids (gr)   : {$this->weight}\n";
+        $res = $res."*\n" ;
+        $res = $res."*  Prix         : {$this->price}\n";
+        $res = $res."********************\n";
+        return $res;
+    }
+
+    /**
+     * Méthode permettant de tirer au hasard un nombre entre 0 et 5000 et de l'affecter
+     * à l'attribut poids de l'article.
+     */
+    public function weighing() : void
+    {
+        $pds = rand(0,5000);
+        $this->setWeight($pds);
+    }
+
 }
